3d888e977ca73fb3f4ce9bd8ebb375aea9f07d7c
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-04-20 14:35:55 -0500

    Iterate and print the $posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,11 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <h1>Posts</h1>
+                <ul>
+                @foreach($posts as $post)
+                    <li>{{ $post->title }}</li>
+                @endforeach
+                </ul>
             </div>
         </div>
     </div>
